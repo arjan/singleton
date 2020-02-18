c8c463894431a109b20b1f298a164568ee0e2e7f
Peter Arentsen <peter.arentsen@holder.nl>
2020-02-18 12:13:53 +0100

    Bump minimum elixir version to when dynamic supervisors are added

--- mix.exs
+++ mix.exs
@@ -2,17 +2,19 @@ defmodule Singleton.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :singleton,
-     version: "1.2.0",
-     elixir: "~> 1.3",
-     elixirc_paths: elixirc_paths(Mix.env),
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     description: description(),
-     package: package(),
-     source_url: "https://github.com/arjan/singleton",
-     homepage_url: "https://github.com/arjan/singleton",
-     deps: deps()]
+    [
+      app: :singleton,
+      version: "1.2.0",
+      elixir: "~> 1.6",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/arjan/singleton",
+      homepage_url: "https://github.com/arjan/singleton",
+      deps: deps()
+    ]
   end
 
   defp description do
@@ -20,21 +22,21 @@ defmodule Singleton.Mixfile do
   end
 
   defp package do
-    %{files: ["lib", "mix.exs",
-              "*.md", "LICENSE"],
+    %{
+      files: ["lib", "mix.exs", "*.md", "LICENSE"],
       maintainers: ["Arjan Scherpenisse"],
       licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/arjan/singleton"}}
+      links: %{"GitHub" => "https://github.com/arjan/singleton"}
+    }
   end
 
   # Configuration for the OTP application
   def application do
-    [applications: [:logger],
-     mod: {Singleton, {}}]
+    [applications: [:logger], mod: {Singleton, {}}]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib"]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
     []
